1fb7bfe3c29b5bdede30714c6f00ef261af3b09a
egor.vakulenko <evakule15@gmail.com>
2019-10-22 11:01:17 +0300

    Revert "account operations"
    This reverts commit aba9f68c

--- src/main/java/com/somn/repository/AccountEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.somn.repository;
-
-import com.somn.model.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountEntityRepository extends JpaRepository<AccountEntity, Long> {
-}
