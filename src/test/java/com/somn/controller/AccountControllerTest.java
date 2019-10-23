273f624d2be24540b62532ad4d16ec07c8078bc4
egor.vakulenko <evakule15@gmail.com>
2019-10-23 17:43:24 +0300

    account-fixes

---
+++ src/test/java/com/somn/controller/AccountControllerTest.java
@@ -0,0 +1,82 @@
+package com.somn.controller;
+
+import com.somn.model.AccountEntity;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
+@Sql(value = {"/somntest_init.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/fill_in_db_test.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class AccountControllerTest {
+  
+  @Autowired
+  private MockMvc mockMvc;
+  
+  @Autowired
+  AccountController accountController;
+  
+  @Before
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+  
+  @Before
+  private AccountEntity getAccountEntity() {
+    return new AccountEntity(50050, "active");
+  }
+  
+  @Test
+  void getAllAccounts() throws Exception {
+    this.mockMvc.perform(get("http://localhost:7070/api/v1/accounts"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("[{\"id\":1,\"balance\":50050,\"accountStatus\":\"active\"}," +
+            "{\"id\":2,\"balance\":50050,\"accountStatus\":\"active\"}," +
+            "{\"id\":3,\"balance\":123456789,\"accountStatus\":\"active\"}]"));
+  }
+  
+  @Test
+  void checkBalance() throws Exception {
+    this.mockMvc.perform(get("http://localhost:7070/api/v1/accounts/2"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("{\"id\":2,\"balance\":50050,\"accountStatus\":\"active\"}"));
+  }
+  
+  @Test
+  void deleteAccount() throws Exception {
+    //deleting object
+    this.mockMvc.perform(delete("http://localhost:7070/api/v1/accounts/1"))
+        .andDo(print())
+        .andExpect(status().isNoContent());
+    
+    //checking that object was deleted
+    this.mockMvc.perform(get("http://localhost:7070/api/v1/accounts"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("[{\"id\":2,\"balance\":50050,\"accountStatus\":\"active\"}," +
+            "{\"id\":3,\"balance\":123456789,\"accountStatus\":\"active\"}]"));
+  }
+}
---
+++ src/test/resources/somntest_init.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS FK_USER_ROLE;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    id                 BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name         VARCHAR(100)                      NOT NULL,
+    encrypted_password VARCHAR(255)                      NOT NULL,
+    user_status        VARCHAR(10)                       NOT NULL
+);
+
+CREATE TABLE accounts
+(
+    id             BIGINT      NOT NULL,
+    balance        INT         NOT NULL,
+    account_status VARCHAR(10) NOT NULL,
+    user_id        BIGINT REFERENCES users (id)
+);
+
+CREATE TABLE roles
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    role_name VARCHAR(20)                       NOT NULL
+);
+
+CREATE TABLE FK_USER_ROLE
+(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    user_id BIGINT                            NOT NULL,
+    role_id BIGINT                            NOT NULL,
+    CONSTRAINT FK_USER_ID_USERS FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    CONSTRAINT FK_ROLE_ID_ROLES FOREIGN KEY (role_id)
+        REFERENCES roles (id)
+);
