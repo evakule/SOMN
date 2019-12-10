4d560bdc9d41f0cf1f4562277ee7482bc3e3c5a1
egor.vakulenko <evakule15@gmail.com>
2019-12-10 11:19:57 +0200

    [SOMN-52 Added exception to account deactivating method]

---
+++ src/main/java/com/somn/service/exception/UnableDeactivateAccountException.java
@@ -0,0 +1,6 @@
+package com.somn.service.exception;
+
+public class UnableDeactivateAccountException extends Throwable {
+  public UnableDeactivateAccountException(String unableDeactivateAccountMessage) {
+  }
+}
